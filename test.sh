dd6e7c7d5cb1c1c14fa85547ee479d3ab6cabe9a
CharithAbeysinghe <charithpriyankara@gmail.com>
2021-11-20 22:55:40 +0530

    test

--- test.sh
+++ test.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
-# Author : klcp
-# Script follows here:
 
 echo "What is your name?"
 read PERSON
 echo "Hello, $PERSON"
-date
+pwd
+ls
+mkdir abc
+chmod  -R 777 abc
+touch abc.txt
+echo $$
+a=0
+while [ "$a" -lt 10 ]    # this is loop1
+do
+   b="$a"
+   while [ "$b" -ge 0 ]  # this is loop2
+   do
+      echo -n "$b "
+      b=`expr $b - 1`
+   done
+   echo
+   a=`expr $a + 1`
+done
